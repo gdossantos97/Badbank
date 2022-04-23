a1b2cdab5d9b5949403fd61565e697c7a5c232ca
Gabriel <gabe.cs1997@gmail.com>
2022-04-23 12:31:45 -0400

    touching things up

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { Container, Card, Button } from 'react-bootstrap'
+import pic from '../images/bank.jpg'; 
+import { Link } from 'react-router-dom';
 
 
 
@@ -11,16 +13,16 @@ export default class Home extends Component {
     return (
       <div>
         <Container>
-          <Card style={{ width:'18rem' }}>
-            <Card.Img variant="top" src=""/>
+          <Card className="text-center">
+            <Card.Img variant="top" src={pic} />
               <Card.Body>
                 <Card.Title>Welcome To Bad Bank!</Card.Title>
                 <Card.Text>
                   This is seriously not a good bank, we will post all of your information
-                  so we chose to name ourselves after our true intentions! I mean hey... At 
-                  least we are honest about it...
+                  so we chose to name ourselves after our true intentions, but I mean hey... At 
+                  least we're honest about it.
                 </Card.Text>
-                <Button variant="danger">Enter at your own risk</Button>
+                <Link to="/Createaccount"><Button variant="danger" >Enter at your own risk</Button></Link>
               </Card.Body>
           </Card>
         </Container>
--- src/components/Navbarcomp.js
+++ src/components/Navbarcomp.js
@@ -2,17 +2,16 @@ import React, { Component } from 'react';
 import { Container } from 'react-bootstrap';
 import { Navbar,Nav, } from 'react-bootstrap';
 import {
-  BrowserRouter as HashRouter,
-  Switch,
-  Route,
-  Link
+  BrowserRouter as HashRouter, Link
 } from "react-router-dom";
 
 
 export default class Navbarcomp extends Component {
   render() {
     return (
+
       <div>
+
         <Navbar bg="dark" variant="dark" expand="lg">
           <Container>
             <Navbar.Brand as={Link} to ={"/"}>BadBank</Navbar.Brand>
@@ -29,6 +28,7 @@ export default class Navbarcomp extends Component {
             </Navbar.Collapse>
           </Container>
         </Navbar>
+
       </div>
     )
   }
